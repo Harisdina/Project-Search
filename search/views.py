5640eef0830db657b42ca5fc66b0466f2e5208a5
Harisdina <dhineshdhina625@gmail.com>
2023-11-19 10:42:11 +0530

    The first time

--- search/views.py
+++ search/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup as bs
+import lxml
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup as bs
+
 def search(request):
     if request.method == 'POST':
         search = request.POST['search']
-        url = 'https://www.ask.com/web?q='+search
+        url = 'https://www.bing.com/search?q=' + search
         res = requests.get(url)
-        soup = bs(res.text, 'lxml')
+        soup = bs(res.text, 'html.parser')
 
-        result_listings = soup.find_all('div', {'class': 'PartialSearchResults-item'})
+        result_listings = soup.find_all('li', class_='b_algo')
 
         final_result = []
 
         for result in result_listings:
-            result_title = result.find(class_='PartialSearchResults-item-title').text
-            result_url = result.find('a').get('href')
-            result_desc = result.find(class_='PartialSearchResults-item-abstract').text
+            result_title = result.find('h2').text
+            result_url = result.find('a')['href']
+            result_desc = result.find('p').text
 
             final_result.append((result_title, result_url, result_desc))
 
